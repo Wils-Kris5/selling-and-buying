ce2480a929997b412a0856d27df360aee2569dea
drake.menard <drake.menard@cgi.com>
2022-06-01 02:20:11 -0500

    all the things, bed time tho

---
+++ src/app/update-product/update-product.component.ts
@@ -0,0 +1,29 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Product} from "../products/product";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {ProductService} from "../services/product.service";
+
+
+@Component({
+  selector: 'app-update-product',
+  templateUrl: './update-product.component.html',
+  styleUrls: ['./update-product.component.css']
+})
+export class UpdateProductComponent implements OnInit {
+
+  errorMessage: string ="";
+  product: Product = this.data;
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any, private productService:ProductService,
+              private dialogRef:MatDialogRef<UpdateProductComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  edit(){
+    this.productService.updateProduct(this.product).subscribe({
+      next: ()=>{
+        this.dialogRef.close()
+      }
+    })
+  }
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  accountNumber: number;
+  username: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.accountNumber = 0;
+    this.username ="";
+    this.password="";
+    this.email="";
+  }
+}
